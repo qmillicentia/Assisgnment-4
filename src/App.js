3992eb8d4019b02689df0d4b7b157ed3f1fbc9d2
qmillicentia <quistmillicentia@gmail.com>
2021-03-26 08:49:06 +0000

    my assisgnment 4

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import new_logo from './new_logo.jpg';
+import react from 'react';
+
+import './App.css';
+
+import {SignUp} from './SignUp/SignUp'
+
+import './signupstyle.css';
+
+function App() {
+
+
+  return (
+   
+    <SignUp />
+  );
+}
+
+export default App;
